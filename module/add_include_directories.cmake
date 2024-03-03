c1485ef82380ac2d6d705e9442768c860430a718
Hato1125 <hato.flavius@gmail.com>
2024-03-03 22:32:32 +0900

    chore: Releaseビルド時に最適化オプションを設定するのとインクルードファイルを出力させる

--- module/add_include_directories.cmake
+++ module/add_include_directories.cmake
@@ -1,8 +1,3 @@
-macro(add_source_files TARGET_NAME EXCEPTION)
-  file(GLOB_RECURSE SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/**.${EXCEPTION})
-  target_sources(${TARGET_NAME} PRIVATE ${SOURCES})
-endmacro()
-
 macro(add_include_directories TARGET_NAME PATH)
   file(GLOB SUBDIRECTORIES LIST_DIRECTORIES ON ${PATH}/*)
   foreach(DIRECTORY ${SUBDIRECTORIES})
